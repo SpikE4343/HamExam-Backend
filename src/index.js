a4f6e0c2c1b7cce7d6833f2d03c4257f332042c2
John Rohrssen <johnrohrssen@gmail.com>
2016-08-28 03:12:51 -0700

    Re-add host/port startup code that got lost in the merge

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 'use strict';
 
 const app = require('./app');
-const port = process.env.port || app.get('port');
+const port = process.env.PORT || app.get('port');
+
+console.log( `env.port = ${process.env.PORT}`);
+console.log( `host = ${app.get('host')}`);
+
 const server = app.listen(port);
+
 
 server.on('listening', () =>
   console.log(`Feathers application started on ${app.get('host')}:${port}`)
