30f9543daa2868797898642a217f9dca7492fb89
John Rohrssen <johnrohrssen@gmail.com>
2016-08-28 02:34:46 -0700

    Second CORS attempt

--- src/middleware/allowCrossDomain.js
+++ src/middleware/allowCrossDomain.js
@@ -3,10 +3,10 @@
 module.exports = function(app) {
   return function(req, res, next) {
     // Perform actions
-    // res.header('Access-Control-Allow-Origin', app.get('host'));
-    // res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,PATCH,DELETE');
-    // res.header('Access-Control-Allow-Headers', 'Content-Type');
-    //
-    // next();
+    res.header('Access-Control-Allow-Origin', app.get('cors.whitelist'));
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,PATCH,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    next();
   };
 };
