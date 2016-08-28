cf20b16e14f02b7550a314768f5bc7f7ef40c1c8
John Rohrssen <johnrohrssen@gmail.com>
2016-08-28 02:57:46 -0700

    Remove old files, add cors options

--- src/services/pool/hooks/uploadtest.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-(echo -n "{ \"name\":\"Technican\", \"tag\":\"tech\", \"pool\":\""; (openssl base64 < techpool.txt | \
- perl -pe 's/([^a-zA-Z0-9_.!~*()'\''-])/sprintf("%%%02X", ord($1))/ge'); \
- echo "\"}") | \
- curl http://hamexam.herokuapp.com/pools \-H "Content-Type: application/json" --data-binary "@-"
